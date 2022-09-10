5f3e22c7094789d9485b8a8f07573fe16dad7af9
Brian Smith <brian@dbsoft.org>
2022-09-10 17:06:13 -0500

    iOS/Android: Switch to using extra container mode for mobile platforms. So we can see the file sizes and timestamps, but need to use the context menu for sorting since the columns are not clickable.

--- src/handyftp.c
+++ src/handyftp.c
@@ -7065,6 +7065,11 @@ int dwmain(int argc, char *argv[])
 	if(getenv("DW_DARK_MODE"))
 		dw_feature_set(DW_FEATURE_DARK_MODE, DW_DARK_MODE_FULL);
 
+#ifdef DW_MOBILE
+    /* Enable extra container display on Mobile platforms */
+    dw_feature_set(DW_FEATURE_CONTAINER_MODE, DW_CONTAINER_MODE_EXTRA);
+#endif
+
 	/* Set the application ID so notifications will work */
 	dw_app_id_set("org.dbsoft.handyftp", "HandyFTP");
 
